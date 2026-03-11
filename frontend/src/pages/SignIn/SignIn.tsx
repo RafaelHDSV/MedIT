7985060be2ab42dc5bd273a498d0188a3d2926f2
Rafael Vieira <rafaelvieira1720@gmail.com>
2026-03-10 23:10:21 -0300

    feat: enhance SignIn component with styled layout and update color variables

--- frontend/src/pages/SignIn/SignIn.tsx
+++ frontend/src/pages/SignIn/SignIn.tsx
@@ -1,7 +1,9 @@
-import { Button, Flex, Typography } from 'antd'
+import SignInImage from '@/assets/signin-image.svg'
+import { Button, Flex, Input } from 'antd'
 import { Link, useNavigate } from 'react-router-dom'
 import { routes } from '../../constants/routes'
 import { useAuth } from '../../hooks/useAuth'
+import styles from './SignIn.module.scss'
 
 export default function SignIn() {
   const { login } = useAuth()
@@ -13,13 +15,27 @@ export default function SignIn() {
   }
 
   return (
-    <div>
-      <Typography.Title level={1}>Login</Typography.Title>
+    <div className={styles.content}>
+      <aside className={styles.form}>
+        <div className={styles.title}>
+          <strong>Med</strong>
+          <span>Flow</span>
+        </div>
 
-      <Flex gap={16}>
-        <Button onClick={handleLogin}>Entrar</Button>
-        <Link to={routes.SIGNUP}>Ainda não possui uma conta? Cadastrar-se</Link>
-      </Flex>
+        <Input value='email@teste.com' />
+        <Input value='senha' />
+
+        <Flex gap={16}>
+          <Button onClick={handleLogin}>Entrar</Button>
+          <Link to={routes.SIGNUP}>
+            Ainda não possui uma conta? Cadastrar-se
+          </Link>
+        </Flex>
+      </aside>
+
+      <aside className={styles.image}>
+        <img src={SignInImage} alt='Sign In' />
+      </aside>
     </div>
   )
 }
