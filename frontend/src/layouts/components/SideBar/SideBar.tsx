c0503b92aedaa52b67959c9dcd1a19ed4826b146
Rafael Vieira <rafaelvieira1720@gmail.com>
2026-03-11 20:10:47 -0300

    feat: restructure SideBar component layout with header and icon integration

--- frontend/src/layouts/components/SideBar/SideBar.tsx
+++ frontend/src/layouts/components/SideBar/SideBar.tsx
@@ -1,10 +1,15 @@
+import { ListIcon } from '@phosphor-icons/react'
 import Logo from '../Logo/Logo'
 import styles from './SideBar.module.scss'
 
 function SideBar() {
   return (
     <nav className={styles.nav}>
-      <Logo />
+      <header className={styles.header}>
+        <ListIcon className={styles.icon} size={32} />
+        <Logo fontSize={32} />
+      </header>
+      routes
     </nav>
   )
 }
