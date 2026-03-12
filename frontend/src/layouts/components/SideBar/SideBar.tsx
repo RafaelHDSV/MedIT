a4d44835c39338f1963be3a49a1edc70a0a56fa5
Rafael Vieira <rafaelvieira1720@gmail.com>
2026-03-11 22:20:59 -0300

    feat: enhance SideBar component with improved styling and dynamic link handling

--- frontend/src/layouts/components/SideBar/SideBar.tsx
+++ frontend/src/layouts/components/SideBar/SideBar.tsx
@@ -1,34 +1,38 @@
 import { ListIcon } from '@phosphor-icons/react'
 import Logo from '../Logo/Logo'
 import styles from './SideBar.module.scss'
-import { NavLink } from 'react-router-dom'
-import routes from '@/routes/routes'
+import { NavLink, useLocation } from 'react-router-dom'
 import { ROUTES } from '@/routes/constants'
+import routes from '@/routes/routes'
 
 function SideBar() {
+  const location = useLocation()
+
   return (
     <nav className={styles.nav}>
       <header className={styles.header}>
         <ListIcon className={styles.icon} size={32} />
         <Logo fontSize={32} />
       </header>
-      <ul style={{ listStyle: 'none', padding: 0, margin: 0 }}>
-        {routes.map((route) => (
-          <li key={route.path} style={{ margin: '1rem 0' }}>
-            <NavLink
-              to={route.path}
-              style={({ isActive }) => ({
-                color: isActive ? 'var(--primary-color)' : 'inherit',
-                textDecoration: 'none',
-                fontWeight: isActive ? 'bold' : 'normal',
-                fontSize: '1.1rem'
-              })}
-              end={route.path === ROUTES.DASHBOARD.path}
-            >
-              {route.name}
-            </NavLink>
-          </li>
-        ))}
+
+      <ul className={styles.menuList}>
+        {routes
+          .filter((route) => route.authed)
+          .map((route) => (
+            <li key={route.path}>
+              <NavLink
+                to={route.path}
+                className={({ isActive }) =>
+                  isActive || location.pathname === route.path
+                    ? styles.activeLink
+                    : styles.link
+                }
+                end={route.path === ROUTES.DASHBOARD.path}
+              >
+                {route.name}
+              </NavLink>
+            </li>
+          ))}
       </ul>
     </nav>
   )
--- frontend/src/routes/routes.ts
+++ frontend/src/routes/routes.ts
@@ -2,19 +2,23 @@ import { ROUTES } from './constants'
 
 const signIn = {
   name: ROUTES.SIGNIN.name,
-  path: ROUTES.SIGNIN.path
+  path: ROUTES.SIGNIN.path,
+  authed: false
 }
 const signUp = {
   name: ROUTES.SIGNUP.name,
-  path: ROUTES.SIGNUP.path
+  path: ROUTES.SIGNUP.path,
+  authed: false
 }
 const dashboard = {
   name: ROUTES.DASHBOARD.name,
-  path: ROUTES.DASHBOARD.path
+  path: ROUTES.DASHBOARD.path,
+  authed: true
 }
 const exampleTable = {
   name: ROUTES.EXAMPLE_TABLE.name,
-  path: ROUTES.EXAMPLE_TABLE.path
+  path: ROUTES.EXAMPLE_TABLE.path,
+  authed: true
 }
 
 const routes = [signIn, signUp, dashboard, exampleTable]
