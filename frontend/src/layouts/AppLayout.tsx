a6b619c56f75d6c0a6858b3d05c6d46d31ddb271
Rafael Vieira <rafaelvieira1720@gmail.com>
2026-03-15 20:01:49 -0300

    fix: correct import path for SideBar component and adjust styling in AppLayout

--- frontend/src/layouts/AppLayout.tsx
+++ frontend/src/layouts/AppLayout.tsx
@@ -1,5 +1,5 @@
+import SideBar from '@/components/SideBar/SideBar'
 import { Outlet } from 'react-router-dom'
-import SideBar from '../components/SideBar/Sidebar'
 import styles from './AppLayout.module.scss'
 
 export default function AppLayout() {
