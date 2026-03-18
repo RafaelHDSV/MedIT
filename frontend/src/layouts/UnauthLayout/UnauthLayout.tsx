1c83074086424e61cd0f9dbbe8496c894a767781
Rafael Vieira <rafaelvieira1720@gmail.com>
2026-03-18 05:10:47 -0300

    feat: refactor UnauthLayout and SignIn components with improved layout and styling

--- frontend/src/layouts/UnauthLayout/UnauthLayout.tsx
+++ frontend/src/layouts/UnauthLayout/UnauthLayout.tsx
@@ -1,5 +1,33 @@
+import UnauthImage from '@/assets/unauth-image.svg'
+import Logo from '@/components/Logo/Logo'
+import { useState } from 'react'
 import { Outlet } from 'react-router-dom'
+import styles from './UnauthLayout.module.scss'
 
 export default function UnauthLayout() {
-  return <Outlet />
+  const [imageIndex, setImageIndex] = useState(0)
+  const imageSource = [UnauthImage, '/SignIn.png']
+
+  function handleImageClick() {
+    setImageIndex((imageIndex + 1) % imageSource.length)
+  }
+
+  return (
+    <div className={styles.content}>
+      <aside className={styles.form}>
+        <Logo />
+
+        <Outlet />
+      </aside>
+
+      <aside className={styles.image}>
+        <img
+          src={imageSource[imageIndex]}
+          alt='Ilustração MedFlow'
+          onClick={handleImageClick}
+        />
+        <h3>Plataforma de Apoio à Triagem e Fluxo Hospitalar</h3>
+      </aside>
+    </div>
+  )
 }
