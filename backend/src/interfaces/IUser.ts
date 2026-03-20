d43353980105adbddaf4d2bfd36a6811d2a0d58e
Rafael Vieira <rafaelvieira1720@gmail.com>
2026-03-19 21:16:07 -0300

    feat: refactor BloodType definition in IUser interface to use an object structure for better clarity

--- backend/src/interfaces/IUser.ts
+++ backend/src/interfaces/IUser.ts
@@ -39,8 +39,17 @@ interface INurse {
   shift?: string
 }
 
-const BloodType = ['O+', 'O-', 'A+', 'A-', 'B+', 'B-', 'AB+', 'AB-'] as const
-type BloodType = (typeof BloodType)[number]
+const BloodType = {
+  A_POSITIVE: 'A+',
+  A_NEGATIVE: 'A-',
+  B_POSITIVE: 'B+',
+  B_NEGATIVE: 'B-',
+  AB_POSITIVE: 'AB+',
+  AB_NEGATIVE: 'AB-',
+  O_POSITIVE: 'O+',
+  O_NEGATIVE: 'O-'
+} as const
+type BloodType = (typeof BloodType)[keyof typeof BloodType]
 
 interface IPatient {
   weight: number
