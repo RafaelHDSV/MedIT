4a81c7d8c514a66bcd272071de8a5bc46af45e48
Rafael Vieira <rafaelvieira1720@gmail.com>
2026-03-21 18:43:18 -0300

    feat: allow additional props to be passed to Button component

--- frontend/src/components/Button/Button.tsx
+++ frontend/src/components/Button/Button.tsx
@@ -1,13 +1,14 @@
 import { Button as AntdButton, type ButtonProps } from 'antd'
 import styles from './Button.module.scss'
 
-function Button({ htmlType, loading, children }: ButtonProps) {
+function Button({ htmlType, loading, children, ...props }: ButtonProps) {
   return (
     <AntdButton
       className={styles.button}
       type='primary'
       loading={loading}
       htmlType={htmlType}
+      {...props}
     >
       {children}
     </AntdButton>
