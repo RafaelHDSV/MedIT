c80bab4c1b10c895081f6958955303557b7e086f
Rafael Vieira <rafaelvieira1720@gmail.com>
2026-03-22 23:53:34 -0300

    feat: create AntdConfigProvider component for centralized Ant Design configuration

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,33 +1,20 @@
-import { theme as antdTheme, ConfigProvider } from 'antd'
-import dayjs from 'dayjs'
-import utc from 'dayjs/plugin/utc'
 import { Suspense } from 'react'
 import { LayoutSpinner } from './components/LayoutSpinner/LayoutSpinner'
+import AntdConfigProvider from './contexts/AntdConfigProvider/AntdConfigProvider'
 import { AuthProvider } from './contexts/AuthContext/AuthProvider'
 import { SettingsProvider } from './contexts/SettingsContext/SettingsProvider'
 import AppRoutes from './routes/AppRoutes'
 
-dayjs.extend(utc)
-
 function App() {
-  const primaryColor = getComputedStyle(document.documentElement)
-    .getPropertyValue('--primary-color')
-    .trim()
-
   return (
     <Suspense fallback={<LayoutSpinner />}>
-      <ConfigProvider
-        theme={{
-          algorithm: antdTheme.defaultAlgorithm,
-          token: { colorPrimary: primaryColor }
-        }}
-      >
+      <AntdConfigProvider>
         <AuthProvider>
           <SettingsProvider>
             <AppRoutes />
           </SettingsProvider>
         </AuthProvider>
-      </ConfigProvider>
+      </AntdConfigProvider>
     </Suspense>
   )
 }
