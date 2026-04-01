8ebf6eeef778f77704cce50335fd17629913cfa6
Rafael Vieira <rafaelvieira1720@gmail.com>
2026-03-31 23:57:46 -0300

    fix: correct import path for MedicationSchema in MedicationModel

--- backend/src/models/MedicationModel.ts
+++ backend/src/models/MedicationModel.ts
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose'
 import { IMedication } from '../interfaces/IMedications'
-import MedicationSchema from '../schema/Medicationschema'
+import MedicationSchema from '../schema/MedicationSchema.js'
 
 export type Medication = mongoose.Model<IMedication>
 export default mongoose.model<IMedication, Medication>(
