5e82212a51b9deb73f61a202e9db4504932d6c5c
Rafael Vieira <rafaelvieira1720@gmail.com>
2026-04-04 22:15:32 -0300

    fix: correct name collection

--- backend/src/models/SymptomsDiseasesModel.ts
+++ backend/src/models/SymptomsDiseasesModel.ts
@@ -3,6 +3,6 @@ import { ISymptomsDiseases } from '../interfaces/ISymptomsDiseases.js'
 import SymptomsDiseaseSchema from '../schema/SymptomsDiseaseSchema.js'
 
 export default mongoose.model<ISymptomsDiseases>(
-  'SympyotsDisease',
+  'SymptomsDisease',
   SymptomsDiseaseSchema
 )
