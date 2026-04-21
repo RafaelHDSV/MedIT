dd4d7207cdf7196ccc1bfa865a29c8234c8e8ccb
Rafael Vieira <rafaelvieira1720@gmail.com>
2026-04-21 18:02:30 -0300

    style: format disabledDate function in MultiDatepicker for improved readability

--- frontend/src/components/MultiDatepicker/MultiDatepicker.tsx
+++ frontend/src/components/MultiDatepicker/MultiDatepicker.tsx
@@ -151,7 +151,8 @@ export default function MultiDatepicker({
       (document.querySelector('.dashboard__filters--date') as HTMLElement) ||
       document.body,
     allowClear,
-    disabledDate: (current: Dayjs) => current.isBefore('2025-01-01') || current.isAfter(dayjs())
+    disabledDate: (current: Dayjs) =>
+      current.isBefore('2025-01-01') || current.isAfter(dayjs())
   }
 
   return (
