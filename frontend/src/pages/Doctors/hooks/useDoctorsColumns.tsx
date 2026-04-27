6dd2f6fb2b7e735d68a509b39883a2f28535a8f0
Rafael Vieira <rafaelvieira1720@gmail.com>
2026-04-26 23:25:17 -0300

    refactor: remove commented code for cleaner implementation in useDoctorsColumns hook

--- frontend/src/pages/Doctors/hooks/useDoctorsColumns.tsx
+++ frontend/src/pages/Doctors/hooks/useDoctorsColumns.tsx
@@ -40,7 +40,6 @@ export function useDoctorsColumns({
     [setEditingDoctor, setEditModalOpen]
   )
 
-  // VIEIRA: Centralizar no DeleteModal
   const handleDelete = useCallback(
     async (doctor: IDoctor) => {
       Modal.confirm({
